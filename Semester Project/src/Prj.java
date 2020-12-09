ef075db59b34da747ccde96140da2d2f89d463a8
Devin Racaniello <devin.racaniello1@marist.edu>
2020-12-09 13:41:42 -0500

    Finished Project
    Added last features and made look beter

--- Semester Project/src/Prj.java
+++ Semester Project/src/Prj.java
@@ -1,12 +1,14 @@
 import java.util.Random;
 import java.lang.Math;
+import java.util.Scanner;
 
 
 public class Prj {
-    private static Person[] testGroup = new Person[1000];
+    public static int amount = 1000;
+    private static Person[] testGroup = new Person[amount];
     private static Random rand = new Random();
     public static String outy = "";
-    public static int posit = 0;
+    public static int pos = 0;
     public static int tests = 0;
     public static int case1 = 0;
     public static int case2 = 0;
@@ -15,37 +17,66 @@ public class Prj {
 
     public static void main(String[] args) {
 
-        sim(1000);
-        split();
-        System.out.println();
-        System.out.println("The infected patients are "+ outy);
+        Scanner comms = new Scanner(System.in);
+        String content = "";
+        double rate  = 2.0;
+
+        System.out.println("Welcome to the AMERICA, I mean my program SuperSpreader.\nPress d, enter to perform a default simulation.\nPress e, enter to edit the settings");
+
+        content = comms.next();
+
+        if(content.equalsIgnoreCase("e")){
+            System.out.println("What population size: ");
+            amount = comms.nextInt();
+            System.out.println("What infection rate (ex 2%=2): ");
+            rate = comms.nextDouble();
+        }
+
+        testGroup = new Person[amount];
+
+        sim(amount,rate);
+        //change name
+        splitTest();
+
+
+
+        //System.out.println();
+        //System.out.println("The infected patients are "+ outy);
 
         int trueTests = case1+(case2*7)+(case3*11);
 
-        System.out.println(trueTests+" number of tests were used");
+
 
 
         double scale = Math.pow(10, 4);
         double case1Percent = Math.round(case1/((double)case2+case3+case1)*scale)/scale,case2Percent = Math.round(case2/((double)case2+case3+case1)*scale)/scale, case3Percent = Math.round(case3/((double)case2+case3+case1)*scale)/scale;
 
+        System.out.println("-------------------------------------------------" );
+
         System.out.println("case 1: "+groups+" x "+case1Percent+" = "+case1+" requiring "+case1+ " tests");
         System.out.println("case 2: "+groups+" x "+case2Percent+" = "+case2+" requiring "+case2*7+ " tests");
         System.out.println("case 3: "+groups+" x "+case3Percent+" = "+case3+" requiring "+case3*11+ " tests" );
 
+        System.out.println("-------------------------------------------------" );
+
+
+        System.out.println(trueTests+" tests used and "+pos+ " infections in a population of "+amount+" people with an infection rate of "+((double)pos/amount)*100+"%.");
     }
 
-    public static void sim(int size){
+    public static void sim(int size,double rate){
         make(size);
-        int rate = 0;
-        int pos = 0;
+        int hasRatio = 0;
         int neg = 0;
 
 
+        double bound = (100/rate);
+        //System.out.println(bound);
+
         for (int i = 0; i < size; i++){
-            rate = rand.nextInt(50);
-            if (rate == 0){
+            hasRatio = rand.nextInt((int)bound);
+            if (hasRatio == 0){
                 testGroup[i].hasCovid = true;
-                System.out.print(i+" ");
+                //System.out.print(i+" ");
                 pos++;
             } else {
                 testGroup[i].hasCovid = false;
@@ -53,7 +84,7 @@ public class Prj {
             }
 
         }
-        System.out.println("Infection rate is " +(1-(double)neg/size));
+        //System.out.println("Infection rate is " +(1-(double)neg/size));
 
 
     }
@@ -64,7 +95,7 @@ public class Prj {
         }
     }
 
-    private static void split(){
+    private static void splitTest(){
         Person[] temp = new Person[8];
 
 
@@ -72,7 +103,7 @@ public class Prj {
             //System.out.print(i%8);
             temp[(i%8)] = testGroup[i];
             if (i%8 == 7){
-                System.out.print(" ");
+                //System.out.print(" ");
                 test(temp);
                 groups+=1;
             }
@@ -91,7 +122,7 @@ public class Prj {
         if (testGroup(group)){
             if (group.length == 8){
 
-                System.out.print("[");
+                //System.out.print("[");
                 for (int i = 0; i < group.length; i++){
 
 
@@ -118,13 +149,13 @@ public class Prj {
 
 
                 }
-                System.out.print("]");
+                //System.out.print("]");
             }
 
 
             int grpCount = 0;
             if (testGroup(lTemp)){
-                System.out.print("[");
+                //System.out.print("[");
                 for (int i = 0; i < lTemp.length; i++){
                     tests++;
 
@@ -132,12 +163,12 @@ public class Prj {
                     if (lTemp[i].hasCovid == true){
                         grpCount++;
                         outy += lTemp[i].id+", ";
-                        System.out.print("*");
+                        //System.out.print("*");
                         //System.out.print(lTemp[i].id+", ");
                     }
-                    System.out.print(lTemp[i].id+" ");
+                    //System.out.print(lTemp[i].id+" ");
                 }
-                System.out.print("]");
+                //System.out.print("]");
 
                 if (grpCount == 1){
                     case2++;
@@ -149,18 +180,18 @@ public class Prj {
 
             grpCount = 0;
             if (testGroup(rTemp)){
-                System.out.print("[");
+                //System.out.print("[");
                 for (int j = 0; j < rTemp.length; j++){
                     tests++;
                     if (rTemp[j].hasCovid == true){
                         grpCount++;
                         outy += rTemp[j].id+", ";
-                        System.out.print("*");
+                        //System.out.print("*");
                         //System.out.print(rTemp[j].id+", ");
                     }
-                    System.out.print(rTemp[j].id+" ");
+                    //System.out.print(rTemp[j].id+" ");
                 }
-                System.out.print("]");
+                //System.out.print("]");
 
                 if (grpCount == 1){
                     case2++;
@@ -189,7 +220,7 @@ public class Prj {
 
 
             if (group[i].hasCovid == true){
-                System.out.print("*");
+                //System.out.print("*");
                 infected = true;
             }
         }
